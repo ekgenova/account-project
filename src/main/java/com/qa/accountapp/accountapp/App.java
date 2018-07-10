7ab0466edfc8fda56ce28f10698912bd96896f0d
Kate <e.genova@live.co.uk>
2018-07-10 10:04:13 +0100

    initial commit

---
+++ src/main/java/com/qa/accountapp/accountapp/App.java
@@ -0,0 +1,10 @@
+package com.qa.accountapp.accountapp;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello world to the standard out" );
+    }
+}
